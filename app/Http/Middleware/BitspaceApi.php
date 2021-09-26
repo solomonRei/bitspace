61bbd98a4dab17592b8a5b698864c72da541d868
solomonRei <45920063+solomonRei@users.noreply.github.com>
2021-09-26 16:13:26 +0300

    Bitspace API: Middleware debug

--- app/Http/Middleware/BitspaceApi.php
+++ app/Http/Middleware/BitspaceApi.php
@@ -16,7 +16,7 @@ class BitspaceApi
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!empty($request->header('Authorization')) || !$request->ajax()) {
+        if (!empty($request->header('Authorization')) && !$request->ajax()) {
             $header = base64_decode(substr($request->header('Authorization'), 6));
             if ($header === config('app.auth_key')) return $next($request);
         }
